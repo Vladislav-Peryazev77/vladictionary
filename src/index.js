4b109457ada6a9ee76b3f5ef5f0ea4db87b34a00
Vladislav-Peryazev77 <vladperyazev14@gmail.com>
2024-05-11 16:39:58 +0700

    test

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import Parse from 'parse/dist/parse.min.js';
+
+// const PARSE_APPLICATION_ID = 'YOUR_APPLICATION_ID_HERE';
+// const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+// const PARSE_JAVASCRIPT_KEY = 'YOUR_JAVASCRIPT_KEY_HERE';
+// Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+// Parse.serverURL = PARSE_HOST_URL;
