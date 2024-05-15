72eb3d1216b42c8692f9b05ee758afd1c204be4c
Vladislav-Peryazev77 <vladperyazev14@gmail.com>
2024-05-15 10:33:01 +0700

    fix

--- src/pages/RegistrationPage/components/Input/RegistrationInput.tsx
+++ src/pages/RegistrationPage/components/Input/RegistrationInput.tsx
@@ -1,6 +1,6 @@
 import { Box, FormLabel, Input } from '@chakra-ui/react';
 import { observer } from 'mobx-react-lite';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 interface RegistrationInputProps {
   labelText: string;
