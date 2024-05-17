2710ee283dd5b43552f9b2be7d31b1b0bcd7bedc
Vladislav-Peryazev77 <vladperyazev14@gmail.com>
2024-05-17 11:33:26 +0700

    initial commit

--- src/pages/DictionaryPage/DictionaryPage.tsx
+++ src/pages/DictionaryPage/DictionaryPage.tsx
@@ -1,3 +1,5 @@
+import { Box } from "@chakra-ui/layout";
+
 export const DictionaryPage = () => {
-  return <div>DictionaryPage</div>;
+  return <Box width='100%' height='100%' display='flex' justifyContent='center' alignItems='center' color='#fff' bgImage="url('src/assets/images/bg-dictionary-test1.jpg')">DictionaryPage</Box>;
 };
