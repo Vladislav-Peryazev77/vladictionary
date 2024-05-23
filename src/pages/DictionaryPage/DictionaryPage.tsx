c370637e92206d6cbe3015671b1c4f29ea3924d5
Vladislav-Peryazev77 <vladperyazev14@gmail.com>
2024-05-23 12:24:06 +0700

    realized the layout for dictionary-quiz-test page with adaptive

--- src/pages/DictionaryPage/DictionaryPage.tsx
+++ src/pages/DictionaryPage/DictionaryPage.tsx
@@ -1,3 +1,116 @@
+import {
+  Box,
+  Button,
+  CloseButton,
+  Icon,
+  Progress,
+  Text,
+} from '@chakra-ui/react';
+import { AnswerVariant } from './components/AnswerVariant';
+
 export const DictionaryPage = () => {
-  return <div>DictionaryPage</div>;
+  return (
+    <Box
+      bgImage="url('src/assets/images/bg-dictionary-test1.jpg')"
+      bgSize="cover"
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      width="100%"
+      height="100%"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        maxWidth="1100px"
+        width="100%"
+        border="2px solid #ffffff80"
+        borderRadius="20px"
+        backdropFilter="blur(150px)"
+        padding={['20px', '20px', '30px', '30px']}
+        color="#fff"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        marginX={['0px', '0px', '20px', '20px']}
+      >
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          marginBottom={['24px', '24px', '90px', '90px']}
+          width="100%"
+        >
+          <Box display="flex" gap="5px" alignItems="center">
+            <img src="src/assets/icons/score-icon.svg" />
+            <Text>200</Text>
+          </Box>
+          <Text fontSize={['18px', '25px']}>Fantasy Quiz #156</Text>
+          <CloseButton />
+        </Box>
+        <Box
+          display={['flex', 'flex', 'none', 'none']}
+          alignItems="center"
+          gap="7px"
+          width={['100%', '70%', '70%', '70%']}
+          marginBottom="40px"
+        >
+          <Progress value={25} width="100%" />
+          <Text>1/5</Text>
+        </Box>
+
+        <Box
+          width="100%"
+          maxWidth="690px"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          textAlign="center"
+          fontSize="26px"
+          marginBottom="80px"
+        >
+          <Text
+            marginBottom={['60px', '60px', '80px', '80px']}
+            fontSize={['22px', '22px', '26px', '26px']}
+          >
+            PREDICT THE TOP LOSER (for tomorrow) across these indices
+          </Text>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap="15px"
+            width="100%"
+            maxWidth="500px"
+          >
+            <AnswerVariant />
+            <AnswerVariant />
+            <AnswerVariant />
+            <AnswerVariant />
+          </Box>
+        </Box>
+        <Box
+          width="100%"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            width={['100%', '70%', '65%', '65%']}
+          >
+            <Box
+              display={['none', 'none', 'flex', 'flex']}
+              alignItems="center"
+              gap="7px"
+            >
+              <Progress value={25} width="200px" />
+              <Text>1/5</Text>
+            </Box>
+            <Button width={['100%', '100%', 'unset', 'unset']}>Continue</Button>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
--- src/pages/TranslationPage/TranslationPage.tsx
+++ src/pages/TranslationPage/TranslationPage.tsx
@@ -6,7 +6,7 @@ export const TranslationPage = () => {
     <Box
       width="100%"
       height="100%"
-      bgImage="url('src/assets/images/bg-translation-page.jpg')"
+      bgImage="url('src/assets/images/bg-transl-test.jpg')"
       bgRepeat="no-repeat"
       bgSize="cover"
       display="flex"
@@ -17,7 +17,7 @@ export const TranslationPage = () => {
         padding={['12px', '15px', '20px', '40px']}
         border="2px solid #ffffff80"
         borderRadius="20px"
-        backdropFilter="blur(12px)"
+        backdropFilter="blur(100px)"
         width="100%"
         maxWidth="1200px"
         color="#fff"
