952740df8539f4e3a5b7598da2a8134a03bc9a01
Vladislav-Peryazev77 <vladperyazev14@gmail.com>
2024-05-27 11:56:42 +0700

    realized the admin panel and a quiz test component

---
+++ src/pages/AdminPanelPage/components/ProtectedAdminPanelRoute/ProtectedAdminPanelRoute.tsx
@@ -0,0 +1,22 @@
+import { observer } from 'mobx-react-lite';
+import { PropsWithChildren } from 'react';
+import RegistrationStore from '../../../../stores/RegistrationStore/RegistrationStore';
+import { Navigate } from 'react-router';
+
+export const ProtectedAdminPanelRoute = observer(
+  ({ children }: PropsWithChildren) => {
+    const { getCurrentUserId, currentUserId } = RegistrationStore;
+    getCurrentUserId();
+    if (
+      currentUserId &&
+      currentUserId.length > 0 &&
+      currentUserId !== 'aZ08EgqfHC'
+    ) {
+      return <Navigate to="/" replace />;
+    }
+    if (currentUserId?.length < 0) {
+      <Navigate to="/login" replace />;
+    }
+    return children;
+  },
+);
